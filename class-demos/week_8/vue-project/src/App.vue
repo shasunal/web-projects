2d6f25ef74dadb7c97d4335b7f2c7f30ead8f68e
Shasuna Lin <162057763+shasunal@users.noreply.github.com>
2025-11-10 16:54:25 -0500

    demo 8

---
+++ class-demos/week_8/vue-project/src/App.vue
@@ -0,0 +1,20 @@
+<script setup>
+import {ref} from "vue"
+const myVariable = ref('hello')
+const greetings = ()=>{
+  console.log('hi')
+}
+onMounted( ()=>{
+  console.log(document.getElementById('blue-paragraph'))
+})
+
+</script>
+
+<template>
+  <button @click = "greetings">{{myVariable}}</button>
+  <p id = "blue-paragraph">
+   now I have some text0-szax
+  </p>
+</template>
+
+<style scoped></style>
